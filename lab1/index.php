25f1ca8a7105f028801ced4d7c35b99f0808d959
Rommel Ormita <52182399+rlormita@users.noreply.github.com>
2019-07-16 00:00:59 +0800

    My site ready for deployment.
    git push heroku master

---
+++ lab1/index.php
@@ -0,0 +1,62 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+<html>
+<head>
+<title>Introduction</title> 
+</head>
+
+<body background="original.gif">
+<font style="font-family:Calibri" size="45" color="D8E4FF">
+
+<font style="font-family:Segoe UI Semibold">
+
+<img src = "latios.gif" align="right" width="300" height="250">
+<h1 style="margin:100px">INTRODUCTION</h1>
+<hr>
+<h2><center> Rommel L. Ormita </center></h2>
+<h3><center> BSIT - MI181 </center></h3>
+</font>
+
+<center><img src = "photo.jpg" width="500" height="500"></center>
+<h5><center><i>This is a picture of me</i><center></h5>
+
+<body>
+
+<img src = "pikachu.gif" align="right" width="250" height="250">
+<p style="margin:150px" align="justify">
+Hello guys! I'm Rommel Ormita and I'm from New Lower Bicutan, Taguig City.<br>
+</p>
+
+<p>
+<p style="margin:150px"   align="justify">
+The story that I gonna tell you with, was the time when I was in SHS and we had a groupings in Novaliches, Quezon City.
+In PUP Main, students come from different cities, and some are from provinces. When we are having groupings, 
+mostly PUP is our meeting place (since it is open from Mon-Sat), because it will be fair to my groupmates to travel.
+But there was a time when all of my groupmates are from the Northern part of Metro Manila, and majority agreed to meet at one of my classmate's house at Novaliches. 
+Of course, the only one being in the south, had no choice but to agree with it.
+It was my first time to travel that far and I don't know how to go there.
+</p>
+<p style="margin:150px" align="justify">
+My classmate gave me instructions which was I need to take MRT and a UV express. 
+Going there was very tiring because it is almost a 2hr travel.
+I was also nervous that time because I might get lost but with the power of asking questions,
+I reached the house of my classmate at Novaliches. But when we need to go home, 
+there was a bus going to Alabang and has a stopover at Bicutan that passes outside their subdivision. 
+I was shocked that my classmate didn't told me this, because it would save money and time for me. 
+So, I took the bus and got home safely, feeling proud that I traveled from Taguig-Novaliches, Novaliches-Taguig.
+</p>
+<p>
+<p style="margin:150px" align="justify">
+On the time when I'm commuting at Novaliches. I am contemplating that if PUP was a good choice to study SHS because it's very tiring to commute. 
+But eventually I realized that it is good choice because I get to go to different places in Metro Manila and Outside Metro Manila.
+</p>
+<p>
+<p style="margin:150px" align="justify">
+I expect that that this class will be very fun and challenging and
+I will fully learn about UI/UX that it will be serve me as a foundation to be a better IT specialist.
+</p>
+
+<center><img src = "mew.gif" width="300" height="250"></center>
+
+<font>
+</body>
+<html>
